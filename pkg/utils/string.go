3ce51f9620ff3c6d8b10ecb03387dba8babe9057
thinkgo <thinkgo@aliyun.com>
2022-12-09 16:09:50 +0800

    fix: fix camel case

--- pkg/utils/string.go
+++ pkg/utils/string.go
@@ -193,13 +193,58 @@ func Kebab(str string, enableLint bool) string {
 	return Recombine(str, '-', enableLint)
 }
 
+// Is c an ASCII lower-case letter?
+func isASCIILower(c byte) bool {
+	return 'a' <= c && c <= 'z'
+}
+
+// Is c an ASCII digit?
+func isASCIIDigit(c byte) bool {
+	return '0' <= c && c <= '9'
+}
+
 // CamelCase to camel case string
 // id_com -> IDCom
 // idcom -> Idcom
 // name_id_com -> NameIDCom
 // name_idcom -> NameIdcom
-func CamelCase(str string, enableLint bool) string {
-	return UnRecombine(str, '_', enableLint)
+func CamelCase(s string, enableLint bool) string {
+	if s == "" {
+		return ""
+	}
+	t := make([]byte, 0, 32)
+	i := 0
+	if s[0] == '_' {
+		// Need a capital letter; drop the '_'.
+		t = append(t, 'X')
+		i++
+	}
+	// Invariant: if the next letter is lower case, it must be converted
+	// to upper case.
+	// That is, we process a word at a time, where words are marked by _ or
+	// upper case letter. Digits are treated as words.
+	for ; i < len(s); i++ {
+		c := s[i]
+		if c == '_' && i+1 < len(s) && isASCIILower(s[i+1]) {
+			continue // Skip the underscore in s.
+		}
+		if isASCIIDigit(c) {
+			t = append(t, c)
+			continue
+		}
+		// Assume we have a letter now - if not, it's a bogus identifier.
+		// The next word is a sequence of characters that must start upper case.
+		if isASCIILower(c) {
+			c ^= ' ' // Make it a capital letter.
+		}
+		t = append(t, c) // Guaranteed not lower case.
+		// Accept lower case sequence that follows.
+		for i+1 < len(s) && isASCIILower(s[i+1]) {
+			i++
+			t = append(t, s[i])
+		}
+	}
+	return string(t)
 }
 
 // SmallCamelCase to small camel case string
