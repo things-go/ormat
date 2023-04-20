f72e30d9db02c3ad5dda73f269ff84a0a1ac163f
thinkgos <thinkgo@aliyun.com>
2023-04-20 14:21:29 +0800

    fix: release v0.5.8-rc1

--- pkg/consts/version.go
+++ pkg/consts/version.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const Version = "v0.5.8-rc0"
+const Version = "v0.5.8-rc1"
 
 func BuildVersion() string {
 	return fmt.Sprintf("%s\nGo Version: %s\nGo Os: %s\nGo Arch: %s\n",
