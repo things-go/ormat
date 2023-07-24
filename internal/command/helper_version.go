c8d3d80c52a3489ef5f98e584c694b262044774d
thinkgos <thinkgo@aliyun.com>
2023-07-24 08:27:46 +0800

    fix: v0.13.0-rc7

--- internal/command/helper_version.go
+++ internal/command/helper_version.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const version = "v0.13.0-rc6"
+const version = "v0.13.0-rc7"
 
 func BuildVersion() string {
 	return fmt.Sprintf("%s\nGo Version: %s\nGo Os: %s\nGo Arch: %s\n",
